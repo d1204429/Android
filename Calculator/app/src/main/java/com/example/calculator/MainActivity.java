4e079a4c0b0dce60ac5d13b76ed5e1a4016762d1
邱勇嘉 <d1204429@o365.fcu.edu.tw>
2024-07-08 10:47:59 +0800

    完成計算機功能

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.calculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,4 +24,47 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    public void addition (View view){
+        TextView result2 =(TextView) findViewById(R.id.result2);
+        EditText input1 = (EditText) findViewById(R.id.input1);
+        EditText input2= (EditText) findViewById(R.id.input2);
+        double one = Double.parseDouble(input1.getText().toString());
+        double two = Double.parseDouble(input2.getText().toString());
+        double temp = (double) (one+two);
+        result2.setText(String.valueOf(temp));
+    }
+
+    public void subtraction (View view){
+        TextView result2 =(TextView) findViewById(R.id.result2);
+        EditText input1 = (EditText) findViewById(R.id.input1);
+        EditText input2= (EditText) findViewById(R.id.input2);
+        double one = Double.parseDouble(input1.getText().toString());
+        double two = Double.parseDouble(input2.getText().toString());
+        double temp = (double) (one-two);
+        result2.setText(String.valueOf(temp));
+    }
+
+    public void multiplication (View view){
+        TextView result2 =(TextView) findViewById(R.id.result2);
+        EditText input1 = (EditText) findViewById(R.id.input1);
+        EditText input2= (EditText) findViewById(R.id.input2);
+        double one = Double.parseDouble(input1.getText().toString());
+        double two = Double.parseDouble(input2.getText().toString());
+        double temp = (double) (one*two);
+        result2.setText(String.valueOf(temp));
+    }
+
+    public void division (View view){
+        TextView result2 =(TextView) findViewById(R.id.result2);
+        EditText input1 = (EditText) findViewById(R.id.input1);
+        EditText input2= (EditText) findViewById(R.id.input2);
+        double one = Double.parseDouble(input1.getText().toString());
+        double two = Double.parseDouble(input2.getText().toString());
+        double temp = (double) (one/two);
+        result2.setText(String.valueOf(temp));
+    }
+
+
+
 }
