6b62f51126d4e22df718e2f5e20c91994b419f92
邱勇嘉 <d1204429@o365.fcu.edu.tw>
2024-07-15 10:31:59 +0800

    finish activity

--- ActivityPractice/app/src/main/java/com/example/activitypractice/SecondActivity.java
+++ ActivityPractice/app/src/main/java/com/example/activitypractice/SecondActivity.java
@@ -1,6 +1,5 @@
 package com.example.activitypractice;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,13 +9,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -24,9 +23,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void GotoSecondActivity(View view){
-        Intent intent = new Intent(this,SecondActivity.class);
-        //帶入
-        startActivity(intent);
+    public void ButtonClick(View view){
+        finish();
     }
+
+
 }
