ca026ea688ba09b26ef337bfe276e0e7f31298f1
邱勇嘉 <d1204429@o365.fcu.edu.tw>
2024-07-15 12:01:34 +0800

    bmi temp

---
+++ BMI/app/src/main/java/com/example/bmi/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.bmi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityResultLauncher<Intent> intentActivityResultLauncher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //設計啟動器啟動intent
+
+        intentActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        //寫成另一個Activity回傳後，得到回傳值之後的作法
+                        if(result.getData() != null && result.getResultCode() == MainActivity.RESULT_OK ){}
+                    }
+                }
+        )
+    }
+
+    public void GotoBNICalculate(View view){
+        Intent intent = new Intent(this, CalBMIAcitivity.class);
+
+
+    }
+}
