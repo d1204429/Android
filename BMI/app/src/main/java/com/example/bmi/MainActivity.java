27ec330b9cbb1dd552f45776383f388198431a3a
邱勇嘉 <d1204429@o365.fcu.edu.tw>
2024-07-17 23:28:58 +0800

    finish BMI 7/17

--- BMI/app/src/main/java/com/example/bmi/MainActivity.java
+++ BMI/app/src/main/java/com/example/bmi/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.bmi;
 
+
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResult;
@@ -15,7 +19,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private ActivityResultLauncher<Intent> intentActivityResultLauncher;
+    private double bmi;
+    private ActivityResultLauncher<Intent> intentActivityResultLanucher;
+    private TextView value;
+    private TextView description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,23 +35,58 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        //設計啟動器啟動intent
-
-        intentActivityResultLauncher = registerForActivityResult(
+        intentActivityResultLanucher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 new ActivityResultCallback<ActivityResult>() {
                     @Override
-                    public void onActivityResult(ActivityResult result) {
-                        //寫成另一個Activity回傳後，得到回傳值之後的作法
-                        if(result.getData() != null && result.getResultCode() == MainActivity.RESULT_OK ){}
+                    public void onActivityResult(ActivityResult o) {
+                        // 這裡寫另一個Activity回傳後，獲取資料並處理的地方
+                        if (o.getData() != null && o.getResultCode() == Activity.RESULT_OK) {
+                            bmi = o.getData().getDoubleExtra("BMI", -1);
+                            updateUI();
+                        }
                     }
                 }
         );
     }
 
-    public void GotoBNICalculate(View view){
-        Intent intent = new Intent(this, CalBMIAcitivity.class);
+    public void GotoCalBMI(View view) {
+        Intent intent = new Intent(this, BMIActivity.class);
 
+        if (value != null) {
+            value.setTextColor(Color.parseColor("#000000"));
+            description.setTextColor(Color.parseColor("#000000"));
+        }
 
+        intentActivityResultLanucher.launch(intent);
+
+    }
+
+    public void updateUI() {
+        TextView tv_result = (TextView) findViewById(R.id.Result);
+        tv_result.setText(String.valueOf(bmi));
+
+        if (bmi < 18.5) {
+            value = (TextView) findViewById(R.id.thin_value);
+            description = (TextView) findViewById(R.id.thin_description);
+        } else if (bmi < 24) {
+            value = (TextView) findViewById(R.id.normal_value);
+            description = (TextView) findViewById(R.id.normal_description);
+        } else if (bmi < 27) {
+            value = (TextView) findViewById(R.id.heavy_value);
+            description = (TextView) findViewById(R.id.heavy_description);
+        } else if (bmi < 30) {
+            value = (TextView) findViewById(R.id.littlefat_value);
+            description = (TextView) findViewById(R.id.littlefat_description);
+        } else if (bmi < 35) {
+            value = (TextView) findViewById(R.id.middlefat_value);
+            description = (TextView) findViewById(R.id.middlefat_description);
+        } else {
+            value = (TextView) findViewById(R.id.toofat_value);
+            description = (TextView) findViewById(R.id.toofat_description);
+        }
+
+        value.setTextColor(Color.parseColor("#CC0000"));
+        description.setTextColor(Color.parseColor("#CC0000"));
     }
 }
