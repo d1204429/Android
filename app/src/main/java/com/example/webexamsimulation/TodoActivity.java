8a5fbcad6ae9425c7a4de34883bdb1492604b1bf
邱勇嘉 <d1204429@o365.fcu.edu.tw>
2024-08-14 02:39:49 +0800

    Update TodoActivity.java
    獲取從 MainActivity 傳遞過來的資料包（Bundle）

--- app/src/main/java/com/example/webexamsimulation/TodoActivity.java
+++ app/src/main/java/com/example/webexamsimulation/TodoActivity.java
@@ -1,12 +1,15 @@
 package com.example.webexamsimulation;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class TodoActivity extends AppCompatActivity {
 
@@ -23,5 +26,21 @@ public class TodoActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // 獲取從 MainActivity 傳遞過來的資料包（Bundle）
+        Bundle bundle = this.getIntent().getExtras();
+        action = bundle.getString("ACTION");
+
+        // 如果動作是編輯，則將資料填入相應的輸入欄位
+        if(bundle != null && action.equals("edit")) {
+            title = String.format(bundle.getString("TITLE"));
+            content = String.format(bundle.getString("CONTENT"));
+            index = bundle.getInt("INDEX");
+
+            EditText tvTitle = (EditText) findViewById(R.id.newTodoTitle);
+            tvTitle.setText(title);
+            TextInputLayout textInputLayout = (TextInputLayout) findViewById(R.id.contentTextInputLayout);
+            textInputLayout.getEditText().setText(content);
+        }
     }
 }
