33bc6099785a0ab013a541e8104e01754c8473d9
邱勇嘉 <d1204429@o365.fcu.edu.tw>
2024-08-14 10:43:33 +0800

    版更前存檔

--- appExam/app/src/main/java/com/example/appexam/Todo.java
+++ appExam/app/src/main/java/com/example/appexam/Todo.java
@@ -3,15 +3,16 @@ package com.example.appexam;
 public class Todo {
     // 成員變數，用來儲存待辦事項的標題與內容
     private String title;
-
-
-
+    private int number;
     private String content;
+    private String img;
 
     // 建構子，用來初始化待辦事項的標題與內容
-    public Todo(String title, String content) {
+    public Todo(String title,int number ,String content,String img) {
         this.title = title;
+        this.number = number;
         this.content = content;
+        this.img = img;
     }
 
     //get與set的方法
@@ -30,4 +31,22 @@ public class Todo {
     public void setContent(String content) {
         this.content = content;
     }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+
+    public String getImg() {
+        return img;
+    }
+
+
 }
